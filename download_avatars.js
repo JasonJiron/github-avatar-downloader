49c3927260a4c17d39e0f4f60afe61df22b34e54
Jason <jason.jiron@gmail.com>
2019-01-22 02:23:35 +0000

    added download_avatars.js

--- download_avatars.js
+++ download_avatars.js
@@ -11,7 +11,6 @@ let getRepoContributors = (repoOwner, repoName, cb) => {
     }
   };
 
-
 getRepoContributors("jquery", "jquery", function(err, result) {
   console.log("Errors:", err);
   console.log("Result:", result);
